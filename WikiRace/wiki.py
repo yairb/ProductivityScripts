18c969155e149c56c8bf43c6149daad7bfcfe475
yair <yair@umoove.me>
2016-07-10 08:33:27 +0300

    add wiki bfs search function

--- WikiRace/wiki.py
+++ WikiRace/wiki.py
@@ -4,7 +4,7 @@ import httplib2, re, urllib, sys
 from bs4 import BeautifulSoup
 
 checkedUrls = []
-def lookForUrl (original, dest, deep, path):
+def naiveLookForUrl (original, dest, deep, path):
 	toRet = 1000
 	if (deep > MAX_DEEP or original in checkedUrls):
 		return toRet
@@ -12,7 +12,7 @@ def lookForUrl (original, dest, deep, path):
 	checkedUrls.append(original)
 	status, response = http.request(original)
 	soup = BeautifulSoup(response, 'html.parser')
-	for link in set(soup.find_all('a', href=re.compile(r"^/wiki/.*[^.jpg]$"))):
+	for link in getAnchors(original):
 		href = link.get('href')
 		#print(href)
 		newList = path[:]
@@ -20,11 +20,39 @@ def lookForUrl (original, dest, deep, path):
 		if (PREFIX + href == dest):
 			print "found!! deep: " + str(deep) + " path: " + str(newList)
 			return deep
-		d = lookForUrl(PREFIX + href, dest, deep + 1, newList)
+		d = naiveLookForUrl(PREFIX + href, dest, deep + 1, newList)
 		if (d < toRet):
 			toRet = d
 	return toRet
-
+def getAnchors (url):
+	print "getAnchors of: 																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								" + url
+	status, response = http.request(url)
+	soup = BeautifulSoup(response, 'html.parser')
+	return set(soup.find_all('a', href=re.compile(r"^/wiki/.*[^.jpg]$")))
+def betterLookForUrl (curList, dest, deep):
+	curDeep = deep
+	found = False
+	while found == False:
+		if (curDeep > MAX_DEEP):
+			return -1
+		print "try deep of " + str(deep) #+ " with path: " + urllib.unquote(str(path)).decode('utf8')
+		#checkedUrls.append(original)
+		nextList = []	
+		for address in curList:
+			if isinstance(address, basestring):
+				url = address
+			else:
+				url = address.get('href')
+			if url.startswith(PREFIX) == False:
+				url = PREFIX + url
+			if url == dest:
+				found = True
+				return deep
+			for a in getAnchors(url):
+				nextList.append(a.get('href'))
+		curDeep += 1
+		curList = nextList
+	return -1
 length = len(sys.argv)
 lang = 'en'
 if length < 3:
@@ -33,8 +61,8 @@ if length < 3:
 if length > 3:
 	lang = str(sys.argv[3])
 PREFIX = 'https://' + lang + '.wikipedia.org'	
-MAX_DEEP = 2
+MAX_DEEP = 3
 http = httplib2.Http()
-PREFIX = 'https://he.wikipedia.org'
-num = lookForUrl(str(sys.argv[1]), str(sys.argv[1]), 0, [])
+#num = naiveLookForUrl(str(sys.argv[1]), str(sys.argv[2]), 0, [])
+num = betterLookForUrl([str(sys.argv[1])], str(sys.argv[2]), 0)
 print "min: " + str(num)
